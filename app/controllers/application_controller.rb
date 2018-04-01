c8f6a29bbae3a127eb3a339a870e2252b049c5ef
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2018-04-01 17:17:56 -0500

    Add hello action to smoke test app.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "<h1>Hello world, from the sample app!</h1>"
+  end
+
 end
